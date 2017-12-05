8ab50645b8305b741de1c52ebde147c1890277a1
Jared Lander <info@jaredlander.com>
2017-12-04 21:16:27 -0500

    add boros

--- code/formulas.r
+++ code/formulas.r
@@ -1,3 +1,12 @@
+boros <- tibble::tribble(
+    ~ Boro, ~ Pop, ~ Size, ~ Random,
+    'Manhattan', 1600000, 23, 7,
+    'Brooklyn', 2600000, 78, 24,
+    'Queens', 2330000, 104, pi,
+    'Bronx', 1455000, 42, 21,
+    'Staten Island', 475000, 60, 3
+)
+
 valueFormula <- TotalValue ~ FireService + ZoneDist1 + ZoneDist2 +
     Class + LandUse + OwnerType + BldgArea + ComArea + ResArea +
     OfficeArea + RetailArea + NumBldgs + NumFloors + LotDepth +
