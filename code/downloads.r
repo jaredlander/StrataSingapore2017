10d7f14a98413691df2dc5d2ddab5f6d8412c08e
Jared Lander <info@jaredlander.com>
2017-12-04 21:03:27 -0500

    Code to download rds files.

---
+++ code/downloads.r
@@ -0,0 +1,6 @@
+download.file('https://www.jaredlander.com/data/manhattan_Train.rds',
+              'data/manhattan_Train.rds')
+download.file('https://www.jaredlander.com/data/manhattan_Validate.rds',
+              'data/manhattan_Validate.rds')
+download.file('https://www.jaredlander.com/data/manhattan_Test.rds',
+              'data/manhattan_Test.rds')
